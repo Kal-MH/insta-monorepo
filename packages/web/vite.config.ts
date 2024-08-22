aa22497210102e7870fb47d49ecb6f57f26f6f1a
Kal-MH <rkfalgus713@gmail.com>
2024-08-22 21:27:54 +0900

    chore: web testing setup

--- packages/web/vite.config.ts
+++ packages/web/vite.config.ts
@@ -1,10 +1,21 @@
+import path from "path";
+
 import { defineConfig } from "vite";
+import eslint from "vite-plugin-eslint";
 import react from "@vitejs/plugin-react";
+
+import type { InlineConfig } from "vitest";
+import type { UserConfig } from "vite";
+
+interface VitestConfigExport extends UserConfig {
+  test: InlineConfig;
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), eslint({ exclude: ["/virtual:/**", "node_modules/**"] })],
   resolve: {
+    alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
     // alias: {
     //   // 경로 alias를 설정하여 디자인 시스템 경로를 쉽게 참조 가능하게 함
     //   "@design-system": path.resolve(
@@ -15,4 +26,9 @@ export default defineConfig({
     // 확장자를 지정하여 .web.tsx 확장자가 먼저 선택되도록 설정
     extensions: [".tsx", ".ts", ".web.tsx", ".web.ts", ".jsx", ".js"],
   },
-});
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./src/utils/test/setupTests.js",
+  },
+} as VitestConfigExport);
