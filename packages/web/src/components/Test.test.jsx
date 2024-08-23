ab61d0ba33d0da14e8915a520cc899c993f062b6
Kal-MH <rkfalgus713@gmail.com>
2024-08-23 10:41:29 +0900

    chore: install frontend packages

--- packages/web/src/components/Test.test.jsx
+++
@@ -1,9 +0,0 @@
-import render from "@/utils/test/render";
-import { screen, within } from "@testing-library/react";
-import Test from "./Test";
-
-it("Test components has 'Test' text", async () => {
-  await render(<Test />);
-
-  expect(screen.getByTestId("test")).toBeInTheDocument();
-});
--- packages/web/src/components/Test.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Test = () => {
-  return <div data-testid="test">Test</div>;
-};
-
-export default Test;
